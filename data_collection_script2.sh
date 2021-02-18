f075ef9866cfeef733efe6f0753433e3bcd9efc8
Jeremy Booher <jeremy.booher@gmail.com>
2021-02-18 14:45:09 +1300

    Periodically Stable Towers

---
+++ data_collection_script2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+run_num="25"
+
+p="3"
+
+for d in 5 8 11
+do
+	echo "load tower_data;  data_collection_periodic_monodromy( $p ,1 , $d, 5 ,24: printing:=true, store:=true, powers:=5);" | magma
+done
+
