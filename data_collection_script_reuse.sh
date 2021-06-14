0e99dc137a74e4607f2863ea66399d9cd069b0c4
Jeremy Booher <jeremy.booher@gmail.com>
2021-06-15 09:35:54 +1200

    Really high powers

--- data_collection_script_reuse.sh
+++ data_collection_script_reuse.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-echo "load tower_data;  further_computations(\"data1\" : powers:=40, printing:=true); further_computations(\"data2\" : powers:=40, printing:=true);" | magma
+echo "load tower_data;  further_computations(\"data1\" : powers:=200, printing:=true); further_computations(\"data2\" : powers:=200, printing:=true);" | magma
 
 
 
